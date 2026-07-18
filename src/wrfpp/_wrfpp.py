a4c1c3796c662738d7633a8718e1955c4beb5554
fazalpsinfo-cmyk <fazalpsinfo@gmail.com>
2026-07-18 22:28:01 +0530

    fix: correct docstring (open_mfdataset -> open_dataset)

--- src/wrfpp/_wrfpp.py
+++ src/wrfpp/_wrfpp.py
@@ -70,7 +70,7 @@ constants = dict(
 
 
 def open_dataset(*args, **kwargs):
-    """Wrapper around xarray.open_mfdataset for WRF output files.
+    """Wrapper around xarray.open_dataset for WRF output files.
 
     Parameters
     ----------
@@ -87,7 +87,7 @@ def open_dataset(*args, **kwargs):
 
 
 def open_mfdataset(paths, **kwargs):
-    """Wrapper around xarray.open_mfdataset for WRF output files.
+    """Wrapper around xarray.open_dataset for WRF output files.
 
     Parameters
     ----------
