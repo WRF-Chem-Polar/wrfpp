e50187303009ce34004518e3aee586a676e90296
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-07-29 11:23:51 +0200

    Add Ruff rule to sort imports in a consistent manner (#28)
    This issue fixes issue #27.

--- src/wrfpp/_wrfpp.py
+++ src/wrfpp/_wrfpp.py
@@ -25,10 +25,11 @@ The WRF model:
 
 """
 
-from abc import ABC, abstractmethod
-import warnings
 import functools
 import re
+import warnings
+from abc import ABC, abstractmethod
+
 import numpy as np
 import scipy
 import xarray as xr
@@ -351,13 +352,11 @@ class GenericDatasetAccessor(ABC):
     @abstractmethod
     def crs_pyproj(self):
         """The CRS (pyproj) corresponding to dataset."""
-        pass
 
     @property
     @abstractmethod
     def crs_cartopy(self):
         """The CRS (cartopy) corresponding to dataset."""
-        pass
 
     @property
     def crs(self):
@@ -1240,7 +1239,6 @@ class DerivedVariable(ABC):
             The derived variable for given slice.
 
         """
-        pass
 
     def __getattr__(self, name):
         return getattr(self[:], name)
